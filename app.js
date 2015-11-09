28cc412469b947f1da23dcd2dd728767e472ebdb
Dariusz Dubicki <devvdi@gmail.com>
2015-11-09 18:50:11 +0100

    Toggle led via button

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var five = require("johnny-five");
 var board = new five.Board();
 
 board.on("ready", function() {
+    var button = new five.Button(2);
     var led = new five.Led(13);
-    led.blink(500);
+    var state = true;
+
+    button.on("press", function() {
+        if (state) {
+            led.on();
+        } else {
+            led.off();
+        }
+
+        state =! state;
+    });
 });
